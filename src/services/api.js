82559eedfd6d287d13a6dbbedd474502f90b5664
hmshreyas26 <hmshreyas26@gmail.com>
2025-04-09 00:23:03 +0100

    added orders component files

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 const API_BASE = 'http://localhost:8080';
 
+
+// <---------------- Product APIs START-------------------------- >
 export const getProducts = async () => {
   const res = await fetch(`${API_BASE}/product`);
   console.log('Fetched products:', res);
@@ -27,3 +29,37 @@ export const updateProduct = async (id, updatedProduct) => {
 export const deleteProduct = async (id) => {
   await fetch(`${API_BASE}/product/${id}`, { method: 'DELETE' });
 };
+
+// <--------------------------------------------- >
+
+// <---------------- Order APIs -------------------------- >
+export const getOrders = async () => {
+  const res = await fetch(`${API_BASE}/order`);
+  console.log('Fetched orders:', res);
+  return await res.json();
+};
+
+export const addOrder = async (Orders) => {
+  const res = await fetch(`${API_BASE}/Orders`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(Orders),
+  });
+  return await res.json();
+};
+
+export const updateOrders = async (id, updatedOrders) => {
+  const res = await fetch(`${API_BASE}/Orders/${id}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(updatedProduct),
+  });
+  return await res.json();
+};
+
+export const deleteOrders = async (id) => {
+  await fetch(`${API_BASE}/Orders/${id}`, { method: 'DELETE' });
+};
+
+
+// <--------------------------------------------- >
