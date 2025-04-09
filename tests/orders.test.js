d62747f267eee6d459c7888f048d01f4778f2ea5
hmshreyas26 <hmshreyas26@gmail.com>
2025-04-09 12:03:04 +0100

    modified code for orders test

--- tests/orders.test.js
+++ tests/orders.test.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const express = require('express');
-const ordersRoute = require('../backend/routes/order');
+const ordersRoute = require('../backend/routes/');
 const app = express();
 
 app.use(express.json());
