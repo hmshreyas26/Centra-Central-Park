5bf76d8858cb4430f932ae6466160b5b70c3eed4
hmshreyas26 <hmshreyas26@gmail.com>
2025-04-09 12:10:23 +0100

    modified code for product.js test

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,7 +6,7 @@ const Products = () => {
   const [form, setForm] = useState({ name: '', category: '', price: '', stock: '' });
 
   const fetchProducts = async () => {
-    const data = await ();
+    const data = await getProducts();
     setProducts(data);
   };
 
